491b399f8ca7d00516d4962fd318bdf6c813f93e
dassoumik <74944139+dassoumik@users.noreply.github.com>
2021-02-26 19:20:27 -0500

    add Engineer class

--- tests/Engineer.test.js
+++ tests/Engineer.test.js
@@ -3,7 +3,7 @@ const Engineer = require("../lib/Engineer");
 
 describe("Engineer class", () => {
     it("Creates an Engineer object", () => {
-        const engineer = new Engineer(1, "Zoltan", "zoltan@fakemail.com");
+        const engineer = new Engineer(1, "Zoltan", "zoltan@fakemail.com", "zoltangit");
 
         expect(engineer.id).toEqual(1);
         expect(engineer.name).toEqual("Zoltan");
@@ -12,24 +12,29 @@ describe("Engineer class", () => {
 
     describe("getName", () => {
         it("Returns the name of the engineer", () => {
-            expect(new Engineer(2, "Jyothi", "jyothi@fakemail.com").getName()).toBe("Jyothi");
+            expect(new Engineer(2, "Jyothi", "jyothi@fakemail.com", "jyothigit").getName()).toBe("Jyothi");
         });
     });
 
     describe("getID", () => {
         it("Returns the ID of the engineer", () => {
-            expect(new Engineer(3, "Faisal", "faisal@fakemail.com").getID()).toBe(3);
+            expect(new Engineer(3, "Faisal", "faisal@fakemail.com", "faisalgit").getID()).toBe(3);
         });
     });
 
     describe("getEmail", () => {
         it("Returns the email of the engineer", () => {
-            expect(new Engineer(4, "Akhil", "akhil@fakemail.com").getEmail()).toBe("akhil@fakemail.com");
+            expect(new Engineer(4, "Akhil", "akhil@fakemail.com", "akhilgit").getEmail()).toBe("akhil@fakemail.com");
         });
     });
     describe("getRole", () => {
         it("Returns the role of the engineer", () => {
-            expect(new Engineer(5, "Karen", "karen@fakemail.com").getRole()).toBe("Engineer");
+            expect(new Engineer(5, "Karen", "karen@fakemail.com", "karengit").getRole()).toBe("Engineer");
+        });
+    });
+    describe("getGitHub", () => {
+        it("Returns the github username of the engineer", () => {
+            expect(new Engineer(5, "Karen", "karen@fakemail.com", "karengit").getGitHub()).toBe("karengit");
         });
     });
 });
